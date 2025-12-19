554c7be0f979b67cfdf18ccd40d544aff232e0a4
sriram-degala <ram.degala2025@gmail.com>
2025-12-18 19:11:12 -0500

    Updated with adding operations as multiply, divide, power

--- src/math_operations.py
+++ src/math_operations.py
@@ -3,3 +3,12 @@ def add(a, b):
 
 def subtract(a, b):
     return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    return a / b
+
+def power(a, b):
+    return a ** b
--- tests/test_operation.py
+++ tests/test_operation.py
@@ -9,3 +9,42 @@ def test_sub():
     assert subtract(4, 3) == 1
     assert subtract(3, 3) == 0
     assert subtract(2, 3) == -1
+
+def test_multiply():
+    assert multiply(2, 3) == 6
+    assert multiply(-1, 1) == -1
+    assert multiply(0, 1) == 0
+    assert multiply(1, 0) == 0
+    assert multiply(1, 1) == 1
+    assert multiply(1, -1) == -1
+    assert multiply(-1, 1) == -1
+    assert multiply(-1, -1) == 1
+    assert multiply(0, 0) == 0
+    assert multiply(0, 1) == 0
+    assert multiply(0, -1) == 0
+
+def test_divide():
+    assert divide(6, 3) == 2
+    assert divide(-1, 1) == -1
+    assert divide(0, 1) == 0
+    assert divide(1, 0) == 0
+    assert divide(1, 1) == 1
+    assert divide(1, -1) == -1
+    assert divide(-1, 1) == -1
+    assert divide(-1, -1) == 1
+    assert divide(0, 0) == 0
+    assert divide(0, 1) == 0
+    assert divide(0, -1) == 0
+
+def test_power():
+    assert power(2, 3) == 8
+    assert power(-1, 1) == -1
+    assert power(0, 1) == 0
+    assert power(1, 0) == 0
+    assert power(1, 1) == 1
+    assert power(1, -1) == -1
+    assert power(-1, 1) == -1
+    assert power(-1, -1) == 1
+    assert power(0, 0) == 0
+    assert power(0, 1) == 0
+    assert power(0, -1) == 0
